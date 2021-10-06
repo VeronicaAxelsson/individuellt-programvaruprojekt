3a48d13055c271e98bd12bb5a2c61c2622003bf9
Veronica <veronica-a@live.se>
2021-10-06 13:02:00 +0200

    no changes, just testing to modify User model

--- vaccinationmanagement/admin.py
+++ vaccinationmanagement/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
+from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Patient, Vaccin, Vaccination
+
+# Register out own model admin, based on the default UserAdmin
+# @admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
+
 
 # Unregister the provided model admin
 admin.site.unregister(User)
-
-# Register out own model admin, based on the default UserAdmin
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    pass
+admin.site.register(User, UserAdmin)
+admin.site.register(Patient)
+admin.site.register(Vaccin)
+admin.site.register(Vaccination)
