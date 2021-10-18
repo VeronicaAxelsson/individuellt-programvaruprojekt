ddc043704971eb928a9864cc3e9aacd431268ba6
Veronica <veronica-a@live.se>
2021-10-18 15:31:58 +0200

    add template tag to get latest vaccination of each kind og vaccin

--- vaccinationmanagement/templatetags/vaccinationmanagement_extras.py
+++ vaccinationmanagement/templatetags/vaccinationmanagement_extras.py
@@ -2,6 +2,17 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='has_group') 
+@register.filter(name='has_group')
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+@register.filter(name='get_item')
+def get_item(dictionary, key):
+    current_vaccin = None
+    for vaccin in dictionary.get(key):
+        if current_vaccin is None:
+            current_vaccin = vaccin
+            continue
+        if vaccin.dose_nr > current_vaccin.dose_nr:
+            current_vaccin = vaccin
+    return [current_vaccin]
